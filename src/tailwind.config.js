3d1ef66ea636f5d367fe6354c255eab4ef0821ec
wendyhamel <whamel@it4web.nl>
2024-04-11 18:16:15 +0200

    First setup mobile styling

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -6,9 +6,11 @@ tailwind.config = {
 		extend: {
 			colors: {
 				'cyan': 'hsl(171, 66%, 44%)',
+				'dark-cyan': 'hsl(167, 34%, 46%)',
 				'blue': 'hsl(233, 100%, 69%)',
-				'dark gray blue': 'hsl(210, 10%, 33%)',
-				'gray blue': 'hsl(201, 11%, 66%)',
+				'dark-blue': 'hsl(233, 59%, 58%)',
+				'dark-gray-blue': 'hsl(210, 10%, 33%)',
+				'gray-blue': 'hsl(201, 11%, 66%)',
 			},
 		}
 	}
