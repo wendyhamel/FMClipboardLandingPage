d654d9bb70d17aef797d0af7e9c09ce80977bff3
wendyhamel <whamel@it4web.nl>
2024-04-11 16:45:09 +0200

    Setup project

---
+++ src/tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+	theme: {
+		fontFamily: {
+			'sans': ['Bai Jamjuree', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				'cyan': 'hsl(171, 66%, 44%)',
+				'blue': 'hsl(233, 100%, 69%)',
+				'dark gray blue': 'hsl(210, 10%, 33%)',
+				'gray blue': 'hsl(201, 11%, 66%)',
+			},
+		}
+	}
+}
