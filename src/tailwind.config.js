9f6217f7c7e382ce77bc3eedb8953dd960ab68d9
wendyhamel <whamel@it4web.nl>
2024-04-12 15:34:51 +0200

    Fixes mobile

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -12,6 +12,14 @@ tailwind.config = {
 				'dark-gray-blue': 'hsl(210, 10%, 33%)',
 				'gray-blue': 'hsl(201, 11%, 66%)',
 			},
+			borderWidth: {
+				'3': '3px'
+			},
+			fontSize: {
+				'1.5xl': ['1.45rem', {lineHeight: '2rem'}],
+				'2.5xl': ['1.675rem', {lineHeight: '2.25rem'}],
+				'3.5xl': ['1.95rem', {lineHeight: '2.5rem'}],
+			}
 		}
 	}
 }
